375073231da50030649414b39a05406d74cdc975
Marshal <marshal.wu@qq.com>
2015-05-21 20:25:05 +0800

    动画基本技术定型

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
-var $ = require('jquery');
-
 $(function() {
-	new Dragend($("#demo").get(0), {
+
+	$("#demo").dragend({
 		afterInitialize: function() {
-			$("#demo").css("visibility", "visible");
-		}
+			$(this.container).css("visibility", "visible");
+			var pages=$('.dragend-page');
+
+			for(var i=0;i<pages.length;i++){
+				if(i>0){
+					$(pages[i]).find('.anim').css('visibility', 'hidden');
+				}
+			}
+		},
+		onSwipeStart:function(container,element,page,direction){
+			this.currentPage=page;
+		},
+		onSwipeEnd:function(container,element,page){
+			if(page!=this.currentPage){
+				$('.dragend-page').find('.anim').css({
+					'visibility':'hidden',
+					'-webkit-animation':''
+				});
+
+				this.settings.animationCallback($(element).find('.anim'));
+			}
+
+			this.currentPage=page;
+		},
+		animationCallback:function(animElements){
+			$(animElements).css('-webkit-animation','fade 0.5s');
+
+			$(animElements).css({
+				visibility:'visible'
+			});
+		}		
 	});
 
-	new Dragend($("#innerDemo").get(0), {
+	$("#innerDemo").dragend({
 		pageClass: "page",
-		stopPropagation: true,
-		onDragStart:function(e){
-			alert(e);
-			// return true;
-		}
+		stopPropagation: true
 	});
 });
